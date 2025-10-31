054baa8e0ac98d6892ed68d149eccc7ac12019c1
SusanketSarkar <susanketsarkar1140@gmail.com>
2025-10-31 20:19:32 +0530

    [Refactor] Update launch function to use Hydra for configuration management

--- pretrain.py
+++ pretrain.py
@@ -532,16 +532,16 @@ def load_synced_config(hydra_config: DictConfig, rank: int, world_size: int) -> 
 
     return objects[0]  # type: ignore
 
-
-def launch(config_path = "config/default.yml"):
+@hydra.main(config_path="config", config_name="cfg_pretrain", version_base=None)
+def launch(hydra_config: DictConfig):
     RANK = 0
     WORLD_SIZE = 1
     CPU_PROCESS_GROUP = None
 
-    with open(config_path, "r") as f:
-        base_cfg = yaml.safe_load(f)
-    from omegaconf import OmegaConf
-    config = OmegaConf.create(base_cfg)
+    # with open(config_path, "r") as f:
+    #     base_cfg = yaml.safe_load(f)
+    # from omegaconf import OmegaConf
+    # config = OmegaConf.create(base_cfg)
 
     # hydra_config['run_name'] = "pretrain_mlp_t_sudoku"
 
@@ -562,7 +562,7 @@ def launch(config_path = "config/default.yml"):
         )
 
     # Load sync'ed config
-    config = load_synced_config(config, rank=RANK, world_size=WORLD_SIZE)
+    config = load_synced_config(hydra_config, rank=RANK, world_size=WORLD_SIZE)
 
     # Seed RNGs to ensure consistency
     torch.random.manual_seed(config.seed + RANK)
